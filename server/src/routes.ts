a23ef20b6588e9a3b13f648b0a7b790fbaddd0a5
Wilson Neto <wilsonpsn@hotmail.com>
2020-06-02 16:01:10 -0300

    fixed migrations

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -5,10 +5,43 @@ const routes = express.Router()
 
 routes.get('/items', async (request, response) => {
   const items = await knex('items').select('*')
+  const serializedItems = items.map(item => {
+    return {
+      id: item.id,
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`
+    }
+  })
+  return response.json(serializedItems)
+})
 
+routes.post('/points', async (req, resp) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items
+  } = req.body
+  try {
+    await knex('points').insert({
+      image: 'image-fake',
+      name,
+      email,
+      whatsapp,
+      latitude,
+      longitude,
+      city,
+      uf
+    })
+  } catch (error) {
+    console.error(error)
+  }
 
-
-  return response.json(items)
+  return resp.json({ success: true })
 })
 
 export default routes
