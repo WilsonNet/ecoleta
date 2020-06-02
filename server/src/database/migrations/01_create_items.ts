97a39d54da71f7893f7b4a19ea343e2c1a6c780b
Wilson Neto <wilsonpsn@hotmail.com>
2020-06-02 12:05:52 -0300

    Databas gerado

---
+++ server/src/database/migrations/01_create_items.ts
@@ -0,0 +1,14 @@
+import Knex from 'knex'
+// (Tipos geralmente é letra maíscula)
+
+export async function up (knex: Knex) {
+  //criar tabela
+
+}
+
+export async function down (knex: Knex) {
+  // Voltar atrás (deletar tabela)
+
+
+}
+
