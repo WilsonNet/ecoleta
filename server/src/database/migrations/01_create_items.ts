3f00858d633bfd038d9e502449caf057aec26428
Wilson Neto <wilsonpsn@hotmail.com>
2020-06-02 12:23:18 -0300

    Done migrations

--- server/src/database/migrations/01_create_items.ts
+++ server/src/database/migrations/01_create_items.ts
@@ -3,12 +3,14 @@ import Knex from 'knex'
 
 export async function up (knex: Knex) {
   //criar tabela
-
+  return knex.schema.createTable('items', table => {
+    table.increments('id').primary()
+    table.string('image').notNullable()
+    table.string('title').notNullable()
+  })
 }
 
 export async function down (knex: Knex) {
   // Voltar atrás (deletar tabela)
-
-
+  return knex.schema.dropTable('point')
 }
-
--- server/src/database/migrations/02_create_point_items.ts
+++ server/src/database/migrations/02_create_point_items.ts
@@ -3,12 +3,25 @@ import Knex from 'knex'
 
 export async function up (knex: Knex) {
   //criar tabela
+  return knex.schema.createTable('points_items', table => {
+    table.increments('id').primary()
 
+    table
+      .integer('point_id')
+      .notNullable()
+      .references('id')
+      .inTable('points')
+
+    table
+      .integer('item_id')
+      .notNullable()
+      .references('id')
+      .inTable('items')
+
+  })
 }
 
 export async function down (knex: Knex) {
   // Voltar atrás (deletar tabela)
- 
-
+  return knex.schema.dropTable('point')
 }
-
