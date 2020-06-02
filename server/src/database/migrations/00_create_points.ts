906f3498dfc40f5ee9cb796deac9d12eda8fb821
Wilson Neto <wilsonpsn@hotmail.com>
2020-06-02 11:58:44 -0300

    Created first migration

---
+++ server/src/database/migrations/00_create_points.ts
@@ -0,0 +1,24 @@
+import Knex from 'knex'
+// (Tipos geralmente é letra maíscula)
+
+export async function up (knex: Knex) {
+  //criar tabela
+  knex.schema.createTable('points', table => {
+    table.increments('id').primary()
+    table.string('image').notNullable()
+    table.string('name').notNullable()
+    table.string('email').notNullable()
+    table.string('whatsapp').notNullable()
+    table.string('latitude').notNullable()
+    table.string('longitude').notNullable()
+    table.string('city').notNullable()
+    table.string('uf', 2).notNullable()
+  })
+}
+
+export async function down (knex: Knex) {
+  // Voltar atrás (deletar tabela)
+  return knex.schema.dropTable('point') 
+
+}
+
