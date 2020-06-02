335b92bd2469a23d03aa4ea5cf4a3d9a30abffd0
Wilson Neto <wilsonpsn@hotmail.com>
2020-06-02 11:35:31 -0300

    Adicionado rota básica

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,36 +4,11 @@ const app = express()
 
 app.use(express.json())
 
-const users = ['Diego', 'Cleiton', 'Robson', 'Hehe']
 
-app.get('/users', (request, response) => {
-  const search = String(request.query.search)
-
-  const filteredUsers = search ? users.filter(user => user.includes(search)): users
-
-  // JSON
-  return response.json(filteredUsers)
+app.get('/', (request, response) => {
+  return response.json('Hello world')
 })
 
-app.get('/users/:id', (request, response)=> {
-  const id = Number(request.params.id)
-
-  const user = users[id]
-
-  return response.json(user)
-
-})
-
-app.post('/users', (request, response) => {
-  const data = request.body;
-  console.log(data)
-  const user = {
-    name: data.name,
-    email: data.email
-  }
-
-  return response.json(user)
-})
 
 
 app.listen(3333)
