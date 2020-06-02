c79e6ac98ac0999418deb1bc0bc77aad76efccb5
Wilson Neto <wilsonpsn@hotmail.com>
2020-06-02 11:26:23 -0300

    Finished search

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,8 +5,21 @@ const app = express()
 const users = ['Diego', 'Cleiton', 'Robson', 'Hehe']
 
 app.get('/users', (request, response) => {
+  const search = String(request.query.search)
+
+  const filteredUsers = search ? users.filter(user => user.includes(search)): users
+
   // JSON
-  return response.json(users)
+  return response.json(filteredUsers)
+})
+
+app.get('/users/:id', (request, response)=> {
+  const id = Number(request.params.id)
+
+  const user = users[id]
+
+  return response.json(user)
+
 })
 
 app.post('/users', (request, response) => {
@@ -17,5 +30,6 @@ app.post('/users', (request, response) => {
 
   return response.json(user)
 })
+
 
 app.listen(3333)
