d72068e83bf5f9013038bd91a6abdea0085a509b
Wilson Neto <wilsonpsn@hotmail.com>
2020-06-06 18:14:56 -0300

    Finished structure

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
-import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native'
-import { Feather as Icon } from '@expo/vector-icons'
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  SafeAreaView,
+} from 'react-native'
+import { Feather as Icon, FontAwesome } from '@expo/vector-icons'
 
-import Contants from 'expo-constants'
 import { useNavigation } from '@react-navigation/native'
+import { RectButton } from 'react-native-gesture-handler'
 
 const Detail = () => {
   const navigation = useNavigation()
@@ -13,19 +20,40 @@ const Detail = () => {
   }
 
   return (
-    <View style={styles.container}>
-      <TouchableOpacity onPress={handleNavigation}>
-        <Icon name="arrow-left" size={20} color="#34cb79" />
-      </TouchableOpacity>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleNavigation}>
+          <Icon name="arrow-left" size={20} color="#34cb79" />
+        </TouchableOpacity>
 
-      <Image
-        style={styles.pointImage}
-        source={{
-          uri:
-            'https://images.unsplash.com/photo-1591035903010-d83ac023ee84?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=80',
-        }}
-      />
-    </View>
+        <Image
+          style={styles.pointImage}
+          source={{
+            uri:
+              'https://images.unsplash.com/photo-1591035903010-d83ac023ee84?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=80',
+          }}
+        />
+
+        <Text style={styles.pointName}>Mercadão do João</Text>
+        <Text style={styles.pointItems}>Mercadão do João</Text>
+
+        <View style={styles.address}>
+          <Text style={styles.address}>Endereço</Text>
+          <Text style={styles.addressContent}>Endereço</Text>
+        </View>
+      </View>
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={() => {}}>
+          <FontAwesome name="whatsapp" size={20} color="#FFF" />
+          <Text style={styles.buttonText}>Whatsapp</Text>
+        </RectButton>
+
+        <RectButton style={styles.button} onPress={() => {}}>
+          <Icon name="mail" size={20} color="#FFF" />
+          <Text style={styles.buttonText}>Whatsapp</Text>
+        </RectButton>
+      </View>
+    </SafeAreaView>
   )
 }
 
